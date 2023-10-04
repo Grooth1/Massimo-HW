5ec385af96074ef4277cdd6af0c117b867643109
Grooth1 <FAL22471@spengergasse.at>
2023-10-04 13:22:36 +0200

    Hase neu

--- Hase.js
+++ Hase.js
@@ -1,18 +1,28 @@
 var bunny = document.querySelector("#bunny");
 var root = document.querySelector("#root");
-var gridColumns = 7; 
-var currentPosition = 0; 
-var gridRow = 7;
-function hop() {
-    
-    
-    currentPosition = (currentPosition + 1) % gridColumns;
-    
 
-    bunny.style.gridColumn = currentPosition + 1; 
-    if (currentPosition == gridColumns) {
-         
-
-    }
+function abc() {
+    let a = 1;
+    let b = 1;
+    let direction = 1; // 1 for moving right, -1 for moving left
+    const avs = setInterval(function () {
+        if (a === 5) {
+            b++;
+            direction = -1;
+            a += direction;
+        } else if (a === 1 && b !== 1) {
+            b++;
+            direction = 1;
+            a += direction;
+        } else {
+            a += direction;
+            
+        }
+        bunny.style.gridColumnStart = a;
+        bunny.style.gridRowStart = b;
+        if (b ===  5 && a === 5) {
+            clearInterval(avs);
+        }
+    }, 400);
+    
 }
-var hopInterval = setInterval(hop, 1000); 
